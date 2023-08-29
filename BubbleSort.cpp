240a5004c54776add53871749bb147969edbd586
Vinayak Ramesh Sutar <vrsdev600@gmail.com>
2023-08-29 20:54:32 +0530

    bubble sort done

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,47 +1,34 @@
-/*
-Bubble Sort Algorithm
-Vinayak Ramesh Sutar
-1459
-*/
 #include <iostream>
 using namespace std;
 
-// print array
-void printArray(int array[], int size)
+int main()
 {
-  for (int i = 0; i < size; ++i)
-  {
-    cout << "  " << array[i];
-  }
-  cout << "\n";
-}
+  int size, temp;
+  int array[] = {54, 74, 12, 32, 52, 14, 11, 23, 25, 65};
 
-void bubbleSort(int array[], int size)
-{
-  int temp;
+  size = sizeof(array) / sizeof(array[0]);
+
+  // bubble sort
   for (int i = 0; i < size; i++)
   {
-    for (int j = 0; j < (size - i) - 1; j++)
+    for (int j = 0; j < size - i; j++)
     {
-      if (array[j] < array[j + 1])
+      if (array[j] > array[j + 1])
       {
+        // swap the elements
         temp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = temp;
       }
     }
   }
-}
 
-int main()
-{
-  int data[] = {54, 12, 36, 25, 48, 57, 41, 52, 54};
+  // printing output
 
-  // find array's length
-  int size = sizeof(data) / sizeof(data[1]);
+  for (int k = 0; k < size; k++)
+  {
+    cout << " " << array[k];
+  }
 
-  bubbleSort(data, size);
-
-  cout << "Sorted Array in Ascending Order:\n";
-  printArray(data, size);
+  return 0;
 }
--- Stack.cpp
+++ Stack.cpp
@@ -4,12 +4,77 @@ Stack
 Vinayak Ramesh Sutar
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+int array[10];
+int top = -1;
+bool takeInput = true;
+int optionIndex;
+
+void push(int value)
+{
+	top++;
+	array[top] = value;
+	cout << "top => " << top;
+}
+
+void pop()
+{
+}
+
+void printArray(int array[], int size)
+{
+	if (size == -1)
+	{
+		cout << "Stack is empty" << endl;
+	}
+	else
+	{
+		cout << "=== Printing Stack ===" << endl;
+		{
+			for (int i = 0; i <= top; ++i)
+				cout << "  " << array[i];
+		}
+	}
+}
 int main()
 {
-	int array[10];
-	
+	while (takeInput)
+	{
+		cout << "\n*** SELECT OPTION ***" << endl;
+		cout << "1.push\n2.pop\n3.print\n4.terminate" << endl;
+		cout << "Option: ";
+		cin >> optionIndex;
+		switch (optionIndex)
+		{
+		case 1:
+		{
+			int element;
+			cout << "Enter element : ";
+			cin >> element;
+			push(element);
+		}
+		break;
+		case 2:
+		{
+			pop();
+		}
+		break;
+		case 3:
+		{
+			printArray(array, top);
+		}
+		break;
+
+		default:
+		{
+
+			takeInput = false;
+			cout << "Program terminated";
+		}
+		}
+	}
+
 	return 0;
 }
